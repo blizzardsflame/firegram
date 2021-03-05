c7552e3c0f4f6c7457f75e4532ce70d24c253910
Redha Nedjari <ned.r@hotmail.fr>
2021-03-05 03:59:56 +0100

    8 - Fix env & ReadMe

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,12 +5,12 @@ import "firebase/firestore";
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyAGRqfHHKsHnta5Ka8JqXOcT3Fpnly_YAQ",
-  authDomain: "react-instafire.firebaseapp.com",
-  projectId: "react-instafire",
-  storageBucket: "react-instafire.appspot.com",
-  messagingSenderId: "350457262472",
-  appId: "1:350457262472:web:691c0ddf149db036da0a3d",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
